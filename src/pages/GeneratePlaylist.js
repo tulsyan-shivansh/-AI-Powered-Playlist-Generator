097801ba0a78fe5cf56c296d0ae9b864f692c52d
SHIVANSH TULSYAN <shivanshtulsyan01@gmail.com>
2025-01-30 02:06:44 +0530

    new changes

---
+++ src/pages/GeneratePlaylist.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Container } from '@mui/material';
+import axios from 'axios';
+
+const GeneratePlaylist = () => {
+    const [mood, setMood] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:5000/api/playlist/generate', {
+                mood,
+            });
+            console.log('Playlist generated successfully:', response.data);
+        } catch (err) {
+            console.error('Error generating playlist:', err);
+        }
+    };
+
+    return (
+        <Container>
+            <Typography variant="h4" gutterBottom>
+                Generate Playlist
+            </Typography>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    label="Mood"
+                    value={mood}
+                    onChange={(e) => setMood(e.target.value)}
+                    fullWidth
+                    margin="normal"
+                />
+                <Button type="submit" variant="contained" color="primary">
+                    Generate
+                </Button>
+            </form>
+        </Container>
+    );
+};
+
+export default GeneratePlaylist;
